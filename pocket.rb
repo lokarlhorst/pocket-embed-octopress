617d03b87f8af25108743797aed5e243f7127859
Philipp Jaeckel <mail@sots.name>
2013-10-19 19:08:58 +0200

    Addes caching support for pocket links

--- pocket.rb
+++ pocket.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'pocket'
+require 'json'
 
 
 module Jekyll
@@ -9,20 +10,15 @@ module Jekyll
       super
       @id = id.strip
 
-      ## Pocket Configuration
-      consumer_key = 'yourconsumerkey'
-      access_token = 'youraccesstoken'
+      @cachefolder = File.expand_path "../.pocketcache", File.dirname(__FILE__)
+      FileUtils.mkdir_p @cachefolder
 
-      Pocket.configure do |config|
-        config.consumer_key = consumer_key
+      if File.exist? get_cached_file(@id)
+        get_cached_data(@id)
+      else
+        get_data_from_pocket(@id)
       end
 
-      client = Pocket.client(:access_token => access_token)
-      info = client.retrieve :detailType => :article
-
-      @title = info["list"][@id]["resolved_title"]
-      @excerpt = info["list"][@id]["excerpt"]
-      @resolved_url = info["list"][@id]["resolved_url"]
     end
 
     def render(context)
@@ -38,6 +34,43 @@ module Jekyll
             ></a><script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
             </div><div style="margin-left: 70px;"><h3>#{@title}</h3></div><div style="margin-left: 70px;"><p>#{@excerpt}</p><p><a href="#{@resolved_url}">#{@resolved_url}</a></p></div></div>)
     end
+
+    def get_data_from_pocket(pocket_id)
+      ## Pocket Configuration
+      consumer_key = 'yourconsumerkey'
+      access_token = 'youraccesstoken'
+      Pocket.configure do |config|
+        config.consumer_key = consumer_key
+      end
+
+      client = Pocket.client(:access_token => access_token)
+      info = client.retrieve :detailType => :article
+
+      @title = info["list"][@id]["resolved_title"]
+      @excerpt = info["list"][@id]["excerpt"]
+      @resolved_url = info["list"][@id]["resolved_url"]
+
+      string = {:title => @title, :excerpt => @excerpt, :resolved_url => @resolved_url}
+      cached_file = get_cached_file(pocket_id)
+
+      File.open(cached_file, "w") do |file|
+        file.write string.to_json
+      end
+    end
+
+    def get_cached_data(pocket_id)
+      cached_file = get_cached_file(pocket_id)
+      info = JSON.parse File.read cached_file if File.exist? cached_file
+      return nil if info.nil?
+      @title = info["title"]
+      @excerpt = info["excerpt"]
+      @resolved_url = info["resolved_url"]
+    end
+
+    def get_cached_file(pocket_id)
+      File.join @cachefolder, "#{pocket_id}.cache"
+    end
+
   end
 end
 
